6cfc684b476a90c19ba117c15710c1e769b899e6
yash goyal <yg364550@gmail.com>
2024-11-29 00:00:09 +0530

    commit

--- controllers/getProductController.js
+++ controllers/getProductController.js
@@ -2,16 +2,18 @@ const { prisma } = require("../db/config");
 
 const getProduct = async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     let fetchedProduct = await prisma.product.findUnique({
-      where: { id: parseInt(id) }
+      where: { id: parseInt(id) },
     });
-    if(!fetchedProduct){
-        return res.status(404).json({message: "Product doesn't exist with specified id"})
+    if (!fetchedProduct) {
+      return res
+        .status(404)
+        .json({ message: "Product doesn't exist with specified id" });
     }
-    return res.status(200).json(fetchedProduct);
+    return res.status(200).json({...fetchedProduct});
   } catch (err) {
-    return res.status(500).json({ "err": err.message });
+    return res.status(500).json({ err: err.message });
   }
 };
 
